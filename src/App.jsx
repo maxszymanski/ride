e28e17ad76079217e2eabe668974d61946619708
Max Szymański <maksymilianszymanski2@gmail.com>
2024-12-03 14:42:34 +0100

    save

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,18 @@
+import Header from './components/Header'
+import StyledDatePicker from './components/StyledDatePicker'
+
 function App() {
-	return <div className="font-open">siema</div>
+	return (
+		<div className="font-open min-h-screen flex flex-col bg-stone-50">
+			<Header />
+			<main className="  py-8  px-6 flex-col h-full flex-1">
+				<div className="flex items-center justify-between">
+					<h2 className="text-center ">Wybierz datę </h2>
+					<StyledDatePicker />
+				</div>
+			</main>
+		</div>
+	)
 }
 
 export default App
---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+function Header() {
+	return (
+		<header className="flex items-center justify-between p-4 border-b border-stone-200">
+			<h1 className="font-semibold  text-stone-950 text-xl  text-center">Dziennik Podróżnika</h1>
+			<button>|||</button>
+		</header>
+	)
+}
+
+export default Header
